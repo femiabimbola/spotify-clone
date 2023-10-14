723f19a944ac13e8cfc009102899febd3960771a
Femi Abimbola <femiabimbola6@gmail.com>
2023-10-14 00:07:48 -0700

    added box component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,9 @@
 
 import { usePathname } from "next/navigation"
 import { useMemo } from "react"
+import {HiHome} from 'react-icons/hi'
+import {BiSearch} from 'react-icons/bi'
+import Box from "@/components/Box"
 
 interface SiderbarProps {
   children: React.ReactNode
@@ -12,10 +15,29 @@ const Sidebar = ({children} : SiderbarProps ) => {
   //  What does useMemo do
 
   const pathname = usePathname()
-  const routes = useMemo(() => [], [] )
+  // pathname is active when they are not on search
+  const routes = useMemo(() => [
+    {
+      icon: HiHome,
+      label: 'Home',
+      active: pathname !== '/search',
+      href:'/'
+    },
+    { 
+      icon: BiSearch,
+      label: 'Search',
+      active: pathname === '/search',
+      href:'/search'
+    }
+  ], [pathname] )
+
   return(
-    <div> 
-      {children}
+    <div className="flex h-full"> 
+     <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+      <Box>
+        {children}
+      </Box>
+     </div>
     </div>
   )
 }
