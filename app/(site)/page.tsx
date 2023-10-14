7f9d09d9fc9859c1218936538036ba4222f519f5
Femi Abimbola <femiabimbola6@gmail.com>
2023-10-14 09:00:40 -0700

    added header and search page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,12 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="text-green-500">
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       The Home Page
-    </main>
+      <Header >
+
+      </Header>
+    </div>
   )
 }
---
+++ app/(site)/search/page.tsx
@@ -0,0 +1,7 @@
+const SearchPage = () => {
+  return(
+    <h1> The search Page</h1>
+  )
+}
+
+export default SearchPage;
