f85b8256426d37d238185e6c2b5a75f49c83dc36
Femi Abimbola <femiabimbola6@gmail.com>
2023-10-21 03:52:53 -0700

    working on modal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,15 +1,57 @@
 "use client";
 
 import Modal from "@/components/Modal";
+import useAuthModal from "@/hooks/useAuthModal";
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useRouter } from "next/navigation";
+
+{
+  /**
+This function displace the registration fields
+It also uses the modal component to follow the pattern 
+of opening and closing the modal.
+So a component responsible for open and closing modal
+This component responsible for details in the modal
+ */
+}
 
 const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
   return (
     <Modal
       title="Welcome back"
       description="Login to your account"
-      isOpen
+      isOpen={isOpen}
       onChange={() => {}}>
-      Auth Modal Children!
+      <Auth
+        theme="dark"
+        magicLink
+        providers={["github", "google"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: { brand: "#404040", brandAccent: "#22c55e" },
+            },
+          },
+        }}
+      />
     </Modal>
   );
 };
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -4,12 +4,15 @@ import AuthModal from "@/components/AuthModal";
 import Modal from "@/components/Modal";
 import { useState, useEffect } from "react";
 
+{
+  /**
+   * Modal start from here
+  Is mounted has to be false because we dont want it load while server is rendering.client side causes hydration reason for next code. None of the modal will be seen during server side rendering
+*/
+}
+
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
-  // Is mounted has to be false because we dont want it load while server is rendering
-
-  // client side causes hydration reason for next code
-  // None of the modal will be seen during server side rendering
   useEffect(() => setIsMounted(true), []);
 
   if (!isMounted) return null;
